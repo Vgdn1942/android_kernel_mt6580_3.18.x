bf4557696c0352b7ad6ed2d403fb6be0b5fc176a
vgdn1942 <vgdn1942@gmail.com>
2018-07-19 20:46:49 +0300

    Update mali gpu drivers to r8p1-00rel0. Test, not work now

--- drivers/gpu/arm/umplock/umplock_driver.c
+++ drivers/gpu/arm/umplock/umplock_driver.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2012-2015 ARM Limited. All rights reserved.
+ * Copyright (C) 2012-2015, 2017 ARM Limited. All rights reserved.
  * 
  * This program is free software and is provided to you under the terms of the GNU General Public License version 2
  * as published by the Free Software Foundation, and any use by you of this program is subject to the terms of such GNU licence.
@@ -14,7 +14,13 @@
 #include <linux/slab.h>
 #include <linux/cdev.h>
 #include <linux/device.h>
+#include <linux/version.h>
+
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(4,12,0)
+#include <linux/uaccess.h>
+#else
 #include <asm/uaccess.h>
+#endif
 #include "umplock_ioctl.h"
 #include <linux/sched.h>
 
